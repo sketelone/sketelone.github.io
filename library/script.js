30159893909b8cfd794d4700732a7d0b77dba817
Sabrina <sketelone@gmail.com>
2022-10-12 11:58:20 -0700

    Update library files

--- library/script.js
+++ library/script.js
@@ -1,32 +1,40 @@
+/*This code stores books from user input into a library and displays the library.
+It also gives users the ability to mark previously stored books as read, 
+as well as remove books.*/
 
-//initialize library
+//intialize constants
 let myLibrary = [];
 
 //create constants for referring to html elements.
+const form = document.getElementById('book-form');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const read = document.getElementById('read');
 const submit = document.getElementById('submit');
 const cardContainer = document.querySelector('.card-container');
+const inputs = document.querySelectorAll("input[type=text]");
 var readButtons = document.getElementsByClassName('read-button');
 var removeButtons = document.getElementsByClassName('remove-button');
 
-//book constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return(this.title + " / " + this.author + ", " + this.pages + " pages, status: " + this.read);
-    }
-}
-
 //when user submits form, add book to the library
 submit.addEventListener('click', function(event) {
-    console.log(read)
-    addBookToLibrary(title, author, pages, read);
+    event.preventDefault();
+    var formValid = false;
+    inputs.forEach(input => {
+        console.log(input, input.validity)
+        if (validate(input) == false) {
+            console.log("nosubmit")
+            formValid = false;
+        } else {
+            formValid = true;
+        }
+    }) 
+    if (formValid == true) {
+        addBookToLibrary(title, author, pages, read);
+        form.reset();
+        closeForm();
+    } 
 })
 
 //when read button is clicked, mark read
@@ -45,9 +53,21 @@ document.addEventListener('click', function(e) {
     }
 })
 
+/*LIBRARY FUNCTIONS*/
+//book constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return(this.title + " / " + this.author + ", " + this.pages + " pages, status: " + this.read);
+    }
+}
+
 //read info from form, construct a new book and add it to the library
 function addBookToLibrary(title, author, pages, read) {
-    if (read.value == "yes") {
+    if (read.checked) {
         var newBook = new Book(title.value, author.value, pages.value, "already read");
     } else {
         var newBook = new Book(title.value, author.value, pages.value, "not read yet");
@@ -72,6 +92,7 @@ function displayLibrary() {
     }
 }
 
+//reset library display
 function reset() {
     console.log("reset")
     while (cardContainer.firstChild) {
@@ -79,18 +100,7 @@ function reset() {
     }
 }
 
-// //display the book in the catalogue
-// function displayBook(book) {
-//     var v = document.createElement('div');
-//     var t = document.createElement('text');
-//     t.textContent = book.info();
-//     v.className = "card";
-//     v.appendChild(t);
-//     addButtons(v);
-//     cardContainer.appendChild(v);
-//     console.log("added new book")
-// }
-
+//add read and remove buttons to each card
 function addButtons(card) {
     var v = document.createElement('div');
     var w = document.createElement("button");
@@ -105,6 +115,7 @@ function addButtons(card) {
     card.appendChild(v);
 }
 
+//mark book as read
 function markRead(entry) {
     var index = parseInt(entry.id);
     myLibrary[index].read = "already read"
@@ -112,6 +123,7 @@ function markRead(entry) {
     entry.firstChild.textContent = myLibrary[index].info();
 }
 
+//remove book from library
 function removeBook(entry) {
     console.log("remove book")
     var index = parseInt(entry.id);
@@ -121,3 +133,58 @@ function removeBook(entry) {
     console.log(temp)
     displayLibrary();
 }
+
+/*FORM CONTROLS*/
+//open form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+//close form
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+/*VALIDATION*/
+//when user input phone number, validate input
+pages.addEventListener('input', (event) => {
+    pages.setCustomValidity("");
+    if (pages.validity.valueMissing) {
+        pages.setCustomValidity("Please fill out this field.");
+    } else if (pages.validity.patternMismatch) {
+        pages.setCustomValidity("Please enter numbers only.");
+    } else {
+        pages.setCustomValidity("");
+    }
+})
+
+//when user inputs anything, validate input
+inputs.forEach(input => {
+    input.addEventListener('input', (event) => {
+        validate(input);
+    })
+})
+
+//show error if input is invalid 
+function validate(i) {
+    console.log(i)
+    if (i.validity.valid) {
+        clearError(i);
+    } else {
+        showError(i);
+        return false
+    }
+}
+
+//clear errors if input is updated to be valid
+function clearError(i) {
+    var inputError = document.querySelector("." + i.name + "_error");
+    inputError.textContent = "";
+}
+
+//show validation message as error
+function showError(i) {
+    var inputError = document.querySelector("." + i.name + "_error");
+    inputError.textContent = i.validationMessage;
+}
+
